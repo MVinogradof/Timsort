aefbdda7eb97cd603b22a1a51f257930d5ddcc9b
MVinogradof <56521578+MVinogradof@users.noreply.github.com>
2019-10-14 02:13:36 +0300

    Rand

--- Timsort/main.cpp
+++ Timsort/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <time.h>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    srand(time(nullptr));
+    int Array[100];
+    for(int i=0; i<100; i++)
+    {
+        Array[i] = rand()%100;
+    }
+    for(int i=0; i<100; i++)
+    {
+        if(i != 0 && i%10 == 0)
+            cout << endl;
+        cout << Array[i] << ' ';
+    }
     return 0;
 }
