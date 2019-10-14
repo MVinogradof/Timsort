eb9adbadf8ba1e1df76801c063a65fe68d371103
MVinogradof <56521578+MVinogradof@users.noreply.github.com>
2019-10-14 18:30:46 +0300

    Insertion sort

--- Timsort/main.cpp
+++ Timsort/main.cpp
@@ -3,19 +3,37 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE 512
+
 using namespace std;
 
 int main()
 {
     srand(time(nullptr));
-    int Array[256];
+    int Array[SIZE];
     int i, j;
     char str[5];
-    for(i=0; i<256; i++)
+    for(i=0; i<SIZE; i++)
     {
         Array[i] = -99 + rand()%200;
     }
-    for(int i=0; i<256; i++)
+
+    int r = 0, n = SIZE;
+    while (n >= 64)
+    {
+        r |= n & 1;
+        n >>= 1;
+    }
+    int minrun = r + n;
+    cout << minrun << endl << endl;
+
+    int currminrun[16][32];
+    for(i=0; i<SIZE; i++)
+    {
+        currminrun[i/32][i%32] = Array[i];
+    }
+
+    for(int i=0; i<SIZE; i++)
     {
         if(i != 0 && i%16 == 0)
             cout << endl;
@@ -23,5 +41,29 @@ int main()
         for(j=0; j<5-strlen(itoa(Array[i], str, 10)); j++)
             cout << ' ';
     }
+    cout << endl <<  "------------------------------------------------------------------------------" << endl;
+
+    for(int k=0; k<16; k++)
+    {
+        for(i=1;i<32;i++)
+        {
+            for(j=i; j>0; j--)
+            {
+                if(currminrun[k][j-1]<currminrun[k][j])
+                    break;
+                swap(currminrun[k][j-1],currminrun[k][j]);
+            }
+        }
+    }
+
+    for(int k=0; k<16; k++)
+    {
+        for(j=0; j<32; j++)
+        {
+            cout << currminrun[k][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
