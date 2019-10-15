62cc9b6692c4c85e2e6f5d42169146376459b8f3
MVinogradof <56521578+MVinogradof@users.noreply.github.com>
2019-10-15 18:40:45 +0300

    Release
    Сделана сортировка run'ов слиянием, Timsort полностью работает

--- Timsort/main.cpp
+++ Timsort/main.cpp
@@ -20,7 +20,7 @@ int main()
     cin >> N;
     int* Array;
     Array = new int[N];
-    int i, j, k, count = 0, start = 0;
+    int i, j, k, l, count = 0, start = 0;
     char str[5];
     for(i=0; i<N; i++)
     {
@@ -35,6 +35,8 @@ int main()
     }
     int minrun = r + n;
     cout << "minrun: " << minrun << endl << endl;
+    cout << "Array:" << endl;
+    cout << "------------------------------------------------------------------------------" << endl;
 
     for(i=0; i<N; i++)
     {
@@ -44,7 +46,7 @@ int main()
         for(j=0; j<static_cast<int>(5-strlen(itoa(Array[i], str, 10))); j++)
             cout << ' ';
     }
-    cout << endl <<  "------------------------------------------------------------------------------" << endl;
+    cout << endl << "------------------------------------------------------------------------------" << endl << endl;
 
     stack <run> Stack;
     run CurrentRun;
@@ -120,19 +122,24 @@ int main()
 
     run* Runs;
     Runs = new run[Stack.size()];
-    i = count = Stack.size() - 1;
+    i = Stack.size() - 1;
+    count = Stack.size();
     while(!Stack.empty())
     {
         Runs[i] = Stack.top();
         i--;
         Stack.pop();
     }
-    for(i=0; i<=count; i++)
+    cout << "runs: (start|size)" << endl;
+    for(i=0; i<count; i++)
     {
         cout << Runs[i].start << ' ' << Runs[i].size << endl;
+        Stack.push(Runs[i]);
     }
+    cout << endl << "Array with sorted runs:" << endl;
+    cout << "------------------------------------------------------------------------------" << endl;
 
-    for(k=0; k<=count; k++)
+    for(k=0; k<count; k++)
     {
         for(i=1; i<Runs[k].size; i++)
         {
@@ -153,7 +160,74 @@ int main()
         for(j=0; j<static_cast<int>(5-strlen(itoa(Array[i], str, 10))); j++)
             cout << ' ';
     }
-    cout << endl <<  "------------------------------------------------------------------------------" << endl;
+    cout << endl << "------------------------------------------------------------------------------" << endl << endl;
+
+    run X, Y;
+    int *temp;
+    int a, b;
+    for(l=0; l<count-1; l++)
+    {
+        Y = Stack.top();
+        Stack.pop();
+        X = Stack.top();
+        Stack.pop();
+        int Size = X.size+Y.size;
+        temp = new int[Size];
+        i = 0;
+        j = 0;
+        for(k=0; k<Size; k++)
+        {
+            if(i > X.size - 1)
+            {
+                a = Array[Y.start+j];
+                temp[k] = a;
+                j++;
+            }
+            else
+            {
+                if(j > Y.size - 1)
+                {
+                    a = Array[X.start+i];
+                    temp[k] = a;
+                    i++;
+                }
+                else
+                {
+                    if(Array[X.start+i] > Array[Y.start+j])
+                    {
+                        a = Array[X.start+i];
+                        temp[k] = a;
+                        i++;
+                    }
+                    else
+                    {
+                        b = Array[Y.start+j];
+                        temp[k] = b;
+                        j++;
+                    }
+                }
+            }
+        }
+        for(k=0; k<Size; k++)
+        {
+            Array[X.start+k] = temp[k];
+        }
+        CurrentRun.start = X.start;
+        CurrentRun.size = X.size + Y.size;
+        Stack.push(CurrentRun);
+    }
+
+    cout << "Array with merged sorted runs:" << endl;
+    cout << "------------------------------------------------------------------------------" << endl;
+    for(i=0; i<N; i++)
+    {
+        if(i != 0 && i%16 == 0)
+            cout << endl;
+        cout << Array[i];
+        for(j=0; j<static_cast<int>(5-strlen(itoa(Array[i], str, 10))); j++)
+            cout << ' ';
+    }
+    cout << endl << "------------------------------------------------------------------------------" << endl;
 
     return 0;
 }
